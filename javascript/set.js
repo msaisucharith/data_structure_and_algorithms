9a0317b44c1af126d973284b01b047ea7edc2e47
VENKATA NAGA SAI SUCHARITH.M <90145299+msaisucharith@users.noreply.github.com>
2023-09-22 17:58:48 +0530

    Add files via upload

---
+++ javascript/set.js
@@ -0,0 +1,27 @@
+// set
+// set is data type which stores only unique values
+// insertion does not make on insertion order
+
+// for creating set we will use constructure
+
+const set = new Set([1, 2, 3, 5]);
+
+// set methods
+
+// add
+set.add(20);
+
+// has
+set.has(20);
+
+// delete
+
+set.delete(20);
+
+// size
+console.log(set.size);
+
+//
+// set.clear();
+
+console.log(set);
